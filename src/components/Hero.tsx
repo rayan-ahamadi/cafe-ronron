8d213aaed31e8c855297daaaeae49fbe75b6e904
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2025-11-18 14:09:01 +0100

    feat: move loading animation to heroSection
    suppression du composant loadingSection pour mettre
    le loader dans le hero et faire une transition plus fluide
    avec le header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,26 +2,36 @@ import { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import LogoCafeRonron from '@assets/images/LogoCafeRonron.svg?react'
+import LogoLoading from '@assets/images/logoLoading.svg?react'
 import Tasse from '@assets/images/tasse.svg?react'
 import MiaouTeam from '@assets/images/miaouTeam.svg?react'
+import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin'
 import { useAnimationStore } from '../AnimationStore'
 
-
+gsap.registerPlugin(MorphSVGPlugin)
 
 function Hero() {
   const tasseRef = useRef(null)
   const catGroupRef = useRef(null)
-  const { loadingHasAnimated } = useAnimationStore()
+  const logoLoadingRef = useRef(null)
+  const headerAddressTextRef = useRef(null)
+  const headerTelTextRef = useRef(null)
+
+  const originalSteamsID = ["steam1-1", "steam2-1", "steam3-1"]
+  const duplicataSteamsID = ["steam1-2", "steam2-2", "steam3-2"]
+
   useGSAP(() => {
+
     const catGroupSelector = gsap.utils.selector(catGroupRef)
     const tasseSelector = gsap.utils.selector(tasseRef)
 
     const catGroup = catGroupSelector('g')
-    const coffeeShadow = tasseSelector('#coffeeShadow')
+    const coffeeShadow = tasseSelector('#coffee-shadow')
     const coffeePattern = tasseSelector('#pattern')
     // const patternPaths = coffeePattern.querySelectorAll('path')
 
-    const opacityTimeline = gsap.timeline()
+    const loadingTimeline = gsap.timeline()
+    //const opacityTimeline = gsap.timeline()
     const textTimeline = gsap.timeline()
     const patternTimeline = gsap.timeline(
       {
@@ -31,52 +41,129 @@ function Hero() {
       }
     )
 
-
-    if (loadingHasAnimated) {
-      // Animation d'apparition des chats et de la tasse
-      opacityTimeline
-        .fromTo(tasseRef.current,
-          { opacity: 0, y: 50, rotate: -30 },
-          { opacity: 1, y: 0, duration: 1, rotate: 0 }
-        )
-        .fromTo(coffeeShadow,
-          { opacity: 0 },
-          { opacity: 1, duration: 1 },
-          '+=0.5'
-        )
-        .fromTo(catGroupRef.current,
-          { opacity: 0, y: 50 },
-          { opacity: 1, y: 0, duration: 1, },
-          '-=0.5'
-        )
-        .fromTo(catGroup,
-          { opacity: 0, y: 20 },
-          { opacity: 1, y: 10, duration: 0.5, stagger: 0.1 },
-          '-=1'
-        )
-
-      // Animation du motif de café
-      patternTimeline
-        .fromTo(coffeePattern,
-          { rotate: 5, transformOrigin: "50% 50%", yoyo: true, repeat: -1 },
-          { rotate: -5, duration: 5, yoyo: true, repeat: -1 }
-        )
-
-    }
-
-  }, [loadingHasAnimated])
+    originalSteamsID.forEach((id, index) => {
+      gsap.to(`#${id}`, {
+        morphSVG: `#${duplicataSteamsID[index]}`,
+        repeat: -1,
+        duration: 0.8,
+        opacity: 0,
+        yoyoEase: "linear.inOut",
+        ease: "linear.inOut",
+        yoyo: true
+      })
+    })
 
 
-  return <section className="hero-pattern h-screen text-text-300 overflow-hidden">
+    const splitCafeGroup = gsap.utils.selector(logoLoadingRef)("#g41 path")
+    const splitRonronGroup = gsap.utils.selector(logoLoadingRef)("#g42 path")
+
+
+    loadingTimeline
+      .fromTo(logoLoadingRef.current,
+        {
+          scale: 0,
+          opacity: 0,
+          top: '50%',
+          left: '50%',
+          xPercent: -50,
+          yPercent: -50,
+        },
+        {
+          scale: 1,
+          opacity: 1,
+          duration: 1.5,
+          ease: 'back.inOut'
+        },
+      )
+      .from(splitCafeGroup, {
+        opacity: 0,
+        ease: "power1.in",
+        y: -20,
+        stagger: 0.1
+      })
+      .from(splitRonronGroup, {
+        opacity: 0,
+        ease: "power1.in",
+        y: -20,
+        stagger: 0.1
+      })
+      .to(logoLoadingRef.current,
+        {
+          scale: 1,
+          width: '5.5rem', // w-22
+          xPercent: -50,  // translate-x-[-50%]
+          yPercent: -50,  // translate-y-[-50%]
+          left: '50%',    // left-1/2
+          top: '3.5rem',  // top-14
+          duration: 1.5,
+          ease: 'power1.out'
+        }
+      )
+      .to(headerAddressTextRef.current,
+        {
+          opacity: 1,
+          duration: 1,
+          ease: 'power1.out'
+        },
+        '>'
+      )
+      .to(headerTelTextRef.current,
+        {
+          opacity: 1,
+          duration: 1,
+          ease: 'power1.out'
+        },
+        '<'
+      )
+      .fromTo(tasseRef.current,
+        {
+          opacity: 0,
+          y: 100,
+          rotate: -100
+        },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1.5,
+          rotate: 0,
+          ease: "power1.out"
+        },
+      )
+      .fromTo(coffeeShadow,
+        { opacity: 0 },
+        { opacity: 0.24, duration: 0.5 },
+        '-=0.5'
+      )
+      .fromTo(catGroup,
+        { opacity: 0, y: "100%", x: 0 },
+        { opacity: 1, y: "5%", x: 0, stagger: 0.3, duration: 0.6, ease: "back.out(1.7)", },
+        '-=1'
+      )
+
+
+
+    // Animation du motif de café
+    patternTimeline
+      .fromTo(coffeePattern,
+        { rotate: 5, transformOrigin: "50% 50%", yoyo: true, repeat: -1, ease: "back.inOut(4)" },
+        { rotate: -5, duration: 5, yoyo: true, repeat: -1, ease: "back.inOut(4)" }
+      )
+
+  })
+
+
+  return <section className="hero-pattern h-screen text-text-300 bg-bg-100 overflow-hidden">
     <header className='flex justify-between items-center px-8 pt-10 font-poppins font-semibold'>
       <div>
-        <p className='text-lg'>37 rue des miaou, Woippy</p>
+        <p className='text-lg opacity-0' ref={headerAddressTextRef}>37 rue des miaou, Woippy</p>
       </div>
-      <div className='absolute translate-x-[-50%] left-1/2 translate-y-[-50%] top-14'>
-        <LogoCafeRonron className='z-0' />
-      </div>
+
+      {/* <LogoCafeRonron className='z-0' /> */}
+      {/* w-22 absolute translate-x-[-50%] left-1/2 translate-y-[-50%] top-14 */}
+      <LogoLoading className='absolute' ref={logoLoadingRef} />
+
       <div>
-        <a href="tel:0422334455" className='text-lg'>04 22 33 44 55</a>
+        <a href="tel:0422334455" className='text-lg opacity-0' ref={headerTelTextRef}>04 22 33 44 55</a>
       </div>
     </header>
 
@@ -88,7 +175,7 @@ function Hero() {
     </div>
     <div>
       <MiaouTeam
-        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full md:h-48 opacity-0"
+        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full md:h-48"
         ref={catGroupRef}
       />
     </div>
